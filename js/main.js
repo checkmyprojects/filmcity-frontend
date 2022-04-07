12c9e52a7fbeb6da3087e6287abb4b1e09c9d7ef
unknown <flaquere@yahoo.es>
2022-04-08 01:01:31 +0200

    parallax effect dont work

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const API_URL = 'http://localhost:8080/movies/'
-// const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-// const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+    // const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+    // const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
 const main = document.getElementById('main')
 const form = document.getElementById('form')
@@ -27,7 +27,7 @@ function showMovies(movies) {
     main.innerHTML = ''
 
     movies.forEach((movie) => {
-        const { title, coverImage, synopsis, score} = movie
+        const { title, coverImage, synopsis, score } = movie
 
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
@@ -48,9 +48,9 @@ function showMovies(movies) {
 }
 
 function getClassByRate(vote) {
-    if(vote >= 4) {
+    if (vote >= 4) {
         return 'green'
-    } else if(vote >= 2) {
+    } else if (vote >= 2) {
         return 'orange'
     } else {
         return 'red'
@@ -58,15 +58,51 @@ function getClassByRate(vote) {
 }
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
+        e.preventDefault()
 
-    const searchTerm = search.value
+        const searchTerm = search.value
 
-    if(searchTerm && searchTerm !== '') {
-        getMoviesSearch(API_URL, searchTerm)
+        if (searchTerm && searchTerm !== '') {
+            getMoviesSearch(API_URL, searchTerm)
 
-        search.value = ''
-    } else {
-        window.location.reload()
+            search.value = ''
+        } else {
+            window.location.reload()
+        }
+    })
+    //animation nave
+const orb = document.querySelector('.orb'),
+    ease = 0.05,
+    start_position = orb.offsetTop;
+let scroll_request = 0,
+    total_offset = 0,
+    animation_running = false;
+
+function animate_scroll() {
+    scroll_request++;
+    if (!animation_running) {
+        animation_running = true;
+        animation_loop();
     }
-})
+}
+scroll_request++;
+
+if (!animation_running) {
+    animation_running = true;
+    animation_loop();
+}
+
+function animation_loop() {
+    let current_offset = window.pageYOffset;
+    let difference = current_offset_total_offset;
+    different *= ease;
+    if (Math.abs(difference) < 0.05) {
+        scroll_request = 0;
+        total_offset = current_offset;
+        animation_running = false;
+        return;
+    }
+    orb.style.top = `${start_position-total_offset}px`;
+    total_offset += difference;
+    requestAnimationFrame(animation_loop);
+}
