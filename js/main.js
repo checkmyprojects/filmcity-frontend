72596a0e87a3575c272545efc8a0039ed18427cd
checkmyprojects <notnow.plz+github@gmail.com>
2022-04-09 22:31:44 +0200

    bugfix

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 const API_URL = 'http://localhost:8080/movies/'
-    // const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
-    // const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
 
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
-// Get initial movies
+// get movies
 getMovies(API_URL)
 
 async function getMovies(url) {
